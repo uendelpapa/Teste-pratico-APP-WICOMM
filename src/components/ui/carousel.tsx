0e44b12bfddab9c0550a1dc3b2d831aa5005524a
uendelpapa <uendelpapa@gmail.com>
2026-07-26 00:10:56 -0300

    feat: add Carousel component based on Embla Carousel and update TS configuration

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -172,7 +172,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 function CarouselPrevious({
   className,
   variant = "outline",
-  size = "icon-sm",
+  size = "icon",
   ...props
 }: React.ComponentProps<typeof Button>) {
   const { orientation, scrollPrev, canScrollPrev } = useCarousel()
@@ -202,7 +202,7 @@ function CarouselPrevious({
 function CarouselNext({
   className,
   variant = "outline",
-  size = "icon-sm",
+  size = "icon",
   ...props
 }: React.ComponentProps<typeof Button>) {
   const { orientation, scrollNext, canScrollNext } = useCarousel()
